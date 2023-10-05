8aa263a844c10d05aa7e5364d5e9edc4a79bb971
tayT0T <pwachara@ucsd.edu>
2023-10-04 22:23:54 -0700

    adding the test function for convertwave

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,19 @@ using HDF5
 parameter = LightMC.readparams()
 ϕps,θps = LightMC.phasePetzold()
 
+η=zeros(parameter.nxs,parameter.nys)
+ηx=zeros(parameter.nxs,parameter.nys)
+ηy=zeros(parameter.nxs,parameter.nys)
+fid=h5open("surfwave.h5","r")
+η0=read(fid,"eta")
+ηx0=read(fid,"ex")
+ηy0=read(fid,"ey")
+close(fid)
+
+convertwave!(η,ηx,ηy,η0,ηx0,ηy0,parameter.kbc)
+
 @testset "LightMC.jl" begin
     @test parameter.nxe == 512
     @test size(ϕps) == (36,)
+    @test ηy[end,end]==ηy[1,1]
 end
