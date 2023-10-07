95a37dc9e0866177231e568f4d3c2c9041a5a2a1
tayT0T <pwachara@ucsd.edu>
2023-10-06 17:12:49 -0700

    try to test the file that we just add serialFFT.jl

--- src/functions.jl
+++ src/functions.jl
@@ -1,6 +1,6 @@
+include("SerialFFT.jl")
 using YAML  #writing the file 
 using Test
-include("SerialFFT.jl")
 using .SerialFFT
 
 "All the Parameters and their attributes that will be used in the simulation"
