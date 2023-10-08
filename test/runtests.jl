64304cc212d3c09becfe2f0eceaded12d34abc75
tayT0T <pwachara@ucsd.edu>
2023-10-07 20:18:52 -0700

    organize the test code by moving to different folder

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,24 +2,10 @@ using LightMC
 using HDF5
 using Test 
 using YAML
-
-parameter = LightMC.readparams()
-ϕps,θps = LightMC.phasePetzold()
-
-η=zeros(parameter.nxs,parameter.nys)
-ηx=zeros(parameter.nxs,parameter.nys)
-ηy=zeros(parameter.nxs,parameter.nys)
-
-fid=h5open("surfwave1.h5","r")
-η0=read(fid,"eta")
-ηx0=read(fid,"ex")
-ηy0=read(fid,"ey")
-close(fid)
-
-LightMC.convertwave!(η,ηx,ηy,η0,ηx0,ηy0,parameter.kbc)
+using MPI
 
 @testset "LightMC.jl" begin
-    @test parameter.nxe == 512
-    @test size(ϕps) == (36,)
-    @test ηy[end,end]==ηy[1,1]
+
+    @info "testing the functions being used in lightMC.jl package"
+    run($test_function.jl)
 end
