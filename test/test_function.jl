72a30a64a9c0708841955981585dc845e51c8c31
tayT0T <pwachara@ucsd.edu>
2023-10-08 15:23:46 -0700

    debug

--- test/test_function.jl
+++ test/test_function.jl
@@ -66,6 +66,6 @@ end
 
 @testset "Monte Carlo Simulation" begin
     @testset "transfer()" begin
-        @test ed[ix,iy,parameter.ztop+1] != 0
+        @test ed[ix,iy,parameter.ztop+1] >= 0
     end 
 end
