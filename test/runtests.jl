2577f0120c69e851600dfff0bb05244d964d58f1
tayT0T <pwachara@ucsd.edu>
2023-12-18 12:02:59 -0800

    changing method for mpi

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,10 +4,22 @@ using YAML
 using MPI
 using UnicodePlots
 
+ncpumax = length(Sys.cpu_info())
+
+ic,jc,kc = [2,2,2]
+if ic * jc * kc > ncpumax
+   ic,jc,kc = [1,1,1]
+end
+
+ncpu = ic * jc * kc
+
+MPI.install_mpiexecjl(force=true)
+mpicmd = joinpath(DEPOT_PATH[1], "bin/mpiexecjl")
+
 @testset "LightMC.jl" begin
     @info "testing the functions being used in lightMC.jl package"
     run(`julia test_function.jl`)
     
-    @info "testing muliple cpu via MPI package"
-    run(`mpirun -n 2 julia test_mpi.jl`)
+    @info "testing with [$ic, $jc, $kc] cpu via MPI package"
+    run(`$mpicmd -n $ncpu julia $(joinpath(@__DIR__, "test_mpi.jl")) $ic $jc $kc`)
 end 
